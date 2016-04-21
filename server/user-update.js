773d7480fb713cacfa6ad7e0d78fc3f07d251c6c
Stephane Bellity <sbellity@gmail.com>
2016-04-21 16:42:56 +0200

    mode debug

--- server/user-update.js
+++ server/user-update.js
@@ -2,13 +2,18 @@ import compute from './compute';
 import _ from 'lodash';
 
 export default function handle({ message }, { ship, hull }) {
-  const { user, segments } = message;
-  const { changes } = compute(message, ship);
+  try {
+    const { user, segments } = message;
+    const { changes } = compute(message, ship);
 
-  if (!_.isEmpty(changes)) {
     if (process.env.DEBUG) {
       console.warn("Apply traits: ", user.id, user.email, JSON.stringify(changes));
     }
-    hull.as(user.id).traits(changes);
+    if (!_.isEmpty(changes)) {
+      hull.as(user.id).traits(changes);
+    }
+  } catch(err) {
+    console.warn('error in compute: ', { err, user, segments });
   }
+
 }
