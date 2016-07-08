0bddbb626a50825c14f09b68228280bdc3844707
Romain Dardour <romain@hull.io>
2016-07-08 18:22:17 +0200

    cleanups

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import Server from "./server";
 
 
 Hull.onLog(function onLog(message, data, ctx = {}) {
-  console.log(`${ctx.id} ] segment.${message}`, JSON.stringify(data || ""));
+  console.log(`${ctx.id} ] processor.${message}`, JSON.stringify(data || ""));
 });
 Hull.onMetric(function onMetric(metric, value, ctx = {}) {
-  console.log(`${ctx.id} ] segment.${metric}`, value);
+  console.log(`${ctx.id} ] processor.${metric}`, value);
 });
 
 Server({
