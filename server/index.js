176218f8f6d335c80bd4b1481e6d385b29baedd8
Stephane Bellity <sbellity@gmail.com>
2016-07-19 19:31:38 +0200

    making it work with newrelic

--- server/index.js
+++ server/index.js
@@ -1,16 +1,72 @@
-/* eslint global-require: 0 */
+import express from "express";
+import path from "path";
+import devMode from "./dev-mode";
+import ComputeHandler from "./compute-handler";
+import responseTime from "response-time";
+import updateUser from "./user-update";
 
-if (process.env.NEW_RELIC_LICENSE_KEY) {
-  console.warn("Starting newrelic agent with key: ", process.env.NEW_RELIC_LICENSE_KEY);
-  require("newrelic");
-}
+module.exports = function Server(options = {}) {
+  const { port, Hull, devMode: dev, hostSecret } = options;
+  const { BatchHandler, NotifHandler, Routes, Middleware } = Hull;
+  const { Readme, Manifest } = Routes;
 
-import Hull from "hull";
-import Server from "./server";
+  const app = express();
 
-Server({
-  Hull,
-  hostSecret: process.env.SECRET || "1234",
-  devMode: process.env.NODE_ENV === "development",
-  port: process.env.PORT || 8082
-});
+  if (dev) app.use(devMode());
+  app.use(responseTime());
+  app.use(express.static(path.resolve(__dirname, "..", "dist")));
+  app.use(express.static(path.resolve(__dirname, "..", "assets")));
+
+  app.set("views", path.resolve(__dirname, "..", "views"));
+
+  app.get("/manifest.json", Manifest(__dirname));
+  app.get("/", Readme);
+  app.get("/readme", Readme);
+
+
+  app.post("/compute", ComputeHandler({ hostSecret, hullClient: Middleware, Hull }));
+  app.post("/batch", BatchHandler({
+    hostSecret,
+    batchSize: 100,
+    groupTraits: false,
+    handler: (notifications = [], { hull, ship }) => {
+      notifications.map(({ message }) => {
+        message.user = hull.utils.groupTraits(message.user);
+        return updateUser({ message }, { hull, ship });
+      });
+    }
+  }));
+  app.post("/notify", NotifHandler({
+    hostSecret,
+    groupTraits: false,
+    onSubscribe() {
+      console.warn("Hello new subscriber !");
+    },
+    handlers: {
+      "user:update": updateUser
+    }
+  }));
+
+  // Error Handler
+  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    if (err) {
+      const data = {
+        status: err.status,
+        segmentBody: req.segment,
+        method: req.method,
+        headers: req.headers,
+        url: req.url,
+        params: req.params
+      };
+      Hull.logger.error("Error ----------------", err.message, err.status, data);
+    }
+
+    return res.status(err.status || 500).send({ message: err.message });
+  });
+
+  Hull.logger.info("started", { port });
+
+  app.listen(port);
+
+  return app;
+};
