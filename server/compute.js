560630cd0693ea8c8e6bbfe953ba1535c02b234c
Michal Raczka <michal@raczka.me>
2017-03-16 12:30:53 +0100

    Fix the array handling

--- server/compute.js
+++ server/compute.js
@@ -189,7 +189,7 @@ module.exports = function compute({ changes = {}, user, segments, events = [] },
       // when we have an array updated we set the whole
       // array in `changed` constant
       if (d.kind === "A") {
-        _.set(memo, d.path, _.get(payload, d.path, []));
+        _.set(memo, d.path, _.get(updatedUser, d.path, []));
       }
       return memo;
     }, {});
