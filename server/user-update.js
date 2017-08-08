683f57cbc65a98a91f4186cc07f17c9b8574c324
Michal Raczka <michal@raczka.me>
2017-08-08 15:16:15 +0200

    Scope the notifications call to user

--- server/user-update.js
+++ server/user-update.js
@@ -63,7 +63,7 @@ module.exports = function handle({ message = {} }, { ship, hull }) {
     }
 
     if (errors && errors.length > 0) {
-      hull.post(`/${ship.id}/notifications`, { status: "error", message: "Script error" });
+      asUser.post(`/${ship.id}/notifications`, { status: "error", message: "Script error" });
       asUser.logger.info("incoming.user.error", { errors, sandbox: true });
     }
 
