f1e4db8be2d7d45a5473e4a752b093acdc520b62
mkawalek <michalkawalekdevkit@icloud.com>
2017-08-10 12:24:04 +0200

    sending errors from status endpoint to platform, cron

--- server/actions/status-check.js
+++ server/actions/status-check.js
@@ -2,7 +2,7 @@ import check from "syntax-error";
 import _ from "lodash";
 
 export default function statusCheck(req, res) {
-  const { ship } = req.hull;
+  const { ship, client } = req.hull;
   const messages = [];
   let status = "ok";
   if (!_.get(ship.private_settings, "code")) {
@@ -16,5 +16,6 @@ export default function statusCheck(req, res) {
     messages.push("Settings are referencing invalid values");
   }
 
-  return res.json({ messages, status });
+  res.json({ messages, status });
+  return client.put(req.hull.ship.id, { status, status_messages: _.uniq(messages) });
 }
