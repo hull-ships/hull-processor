8a39eebf3d538d0126761a0b6b2985cceda31071
benjamin.dahon <benjamin@hull.io>
2017-09-18 11:13:07 +0200

    Exiting process

--- getEnv.js
+++ getEnv.js
@@ -2,14 +2,11 @@
 const fs = require('fs')
 const dotEnvExists = fs.existsSync('.env')
 if (dotEnvExists) {
-  console.log('getEnv.js: .env exists, probably running on development environment')
-  // process.exit()
+  console.log('getEnv.js: .env exists, probably running on development environment');
+  process.exit();
 }
 // On Google Cloud Platform authentication is handled for us
 const gcs = require('@google-cloud/storage')()
-
-NODE_ENV
-
 
 const bucketName = `test-env-var-kms`
 console.log(`Downloading .env from bucket "${bucketName}"`)
@@ -22,4 +19,4 @@ gcs
   })
   .catch(e => {
     console.error(`getEnv.js: There was an error: ${e}`)
-  })
+  });
