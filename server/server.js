a84d38df959424cb3111bf7fd26399757041092e
Michal Raczka <michal@raczka.me>
2017-11-06 15:39:37 +0100

    Cleanup deps and error middleware

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import devMode from "./dev-mode";
 
 export default function Server(connector, options = {}) {
   const app = express();
-  const { Hull, hostSecret } = options;
+  const { hostSecret } = options;
 
   app.post("/compute", computeHandler({ hostSecret, connector }));
 
@@ -17,27 +17,11 @@ export default function Server(connector, options = {}) {
   app.post("/batch", notifyHandler());
   app.post("/smart-notifier", notifyHandler({
     type: "next",
-    size: parseInt(process.env.FLOW_CONTROL_SIZE, 10) || 10,
-    in: parseInt(process.env.FLOW_CONTROL_IN, 10) || 1000
+    size: parseInt(process.env.FLOW_CONTROL_SIZE, 10) || 100,
+    in: parseInt(process.env.FLOW_CONTROL_IN, 10) || 10
   }));
   app.post("/notify", notifyHandler());
   app.all("/status", statusCheck);
 
-  // Error Handler
-  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-    if (err) {
-      const data = {
-        status: err.status,
-        segmentBody: req.segment,
-        method: req.method,
-        headers: req.headers,
-        url: req.url,
-        params: req.params
-      };
-      Hull.logger.error("Error ----------------", err.message, err.status, data, err.stack);
-      return res.status(err.status || 500).send({ message: err.message });
-    }
-    return res.status(err.status || 500).send({ message: "undefined error" });
-  });
   return app;
 }
