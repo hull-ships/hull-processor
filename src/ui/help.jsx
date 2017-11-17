35001c9dfe8770aa8c08fae9ae153a2ef0e1760d
Romain Dardour <romain@hull.io>
2017-11-17 19:29:54 +0100

    fix crash

--- src/ui/help.jsx
+++ src/ui/help.jsx
@@ -9,7 +9,7 @@ export default class Help extends Component {
   render() {
     return (
       <div>
-        <a href='/readme' target="_blank" bsStyle="warning" bsSize="sm" clasName='btn-pill btn-rounded' onClick={this.open.bind(this)} >Documentation</a>
+        <a href='/readme' target="_blank" bsStyle="warning" bsSize="sm" clasName='btn-pill btn-rounded'>Documentation</a>
       </div>
     );
   }
