f70245e16fc359ed4f0ba5a142889e3ef72c7a6b
Stephane Bellity <sbellity@gmail.com>
2018-01-24 10:33:17 +0100

    revert verbose logging

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import compression from "compression";
 
 import computeHandler from "./actions/compute-handler";
@@ -12,17 +11,6 @@ export default function Server(connector, options = {}) {
   const app = express();
   app.use(compression());
   const { hostSecret } = options;
-
-  if (options.debug === "verbose") {
-    app.use(bodyParser.json({ limit: "10mb" }), (req, res, next) => {
-      try {
-        console.log("[smart-notifier-payload]", JSON.stringify({ path: req.path, body: req.body }));
-      } catch (err) {
-        console.log("[smart-notifier-payload] error: ", err);
-      }
-      next();
-    });
-  }
 
   app.post("/compute", computeHandler({ hostSecret, connector }));
 
