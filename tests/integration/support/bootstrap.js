5d7f80f2ad6aee5c34299ad00f1054b1e51d568d
Michal Raczka <michal@raczka.me>
2018-04-05 12:35:30 +0200

    Small cleanup

--- tests/integration/support/bootstrap.js
+++ tests/integration/support/bootstrap.js
@@ -4,7 +4,7 @@ const server = require("../../../server/server");
 module.exports = function bootstrap(timeout = 25000) {
   const hostSecret = "1234";
   const connector = new Hull.Connector({
-    hostSecret, port: 8000, skipSignatureValidation: true, timeout: timeout, clientConfig: { protocol: "http", firehoseUrl: "firehose" }
+    hostSecret, port: 8000, skipSignatureValidation: true, timeout, clientConfig: { protocol: "http", firehoseUrl: "firehose" }
   });
   const app = server(connector, {
     Hull,
